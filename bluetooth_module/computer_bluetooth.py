27ea0eed3d068ccec6f4b31c45b8597d938d290a
Huy_Surface <bicoi99@gmail.com>
2019-11-29 17:27:08 +0000

    Change print statement from 2.7 to 3.7

--- bluetooth_module/computer_bluetooth.py
+++ bluetooth_module/computer_bluetooth.py
@@ -1,26 +1,26 @@
 # This project requires PyBluez
-from Tkinter import *
+from tkinter import *
 import bluetooth
 
 # Look for all Bluetooth devices
 # the computer knows about.
-print "Searching for devices..."
-print ""
+print("Searching for devices...")
+print("")
 # Create an array with all the MAC
 # addresses of the detected devices
 nearby_devices = bluetooth.discover_devices()
 # Run through all the devices found and list their name
 num = 0
-print "Select your device by entering its coresponding number..."
+print("Select your device by entering its coresponding number...")
 for i in nearby_devices:
     num += 1
-    print num, ": ", bluetooth.lookup_name(i)
+    print(num, ": ", bluetooth.lookup_name(i))
 
 # Allow the user to select their Arduino
 # bluetooth module. They must have paired
 # it before hand.
-selection = input("> ") - 1
-print "You have selected", bluetooth.lookup_name(nearby_devices[selection])
+selection = int(input("> ")) - 1
+print("You have selected", bluetooth.lookup_name(nearby_devices[selection]))
 bd_addr = nearby_devices[selection]
 
 port = 1
