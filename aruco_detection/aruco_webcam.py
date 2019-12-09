13232ff8a6313704d829cb3d88ddb5e06a913d9e
Huy Duong <hnd1g17@soton.ac.uk>
2019-12-09 10:37:17 +0000

    Changes to send serial signal via bluetooth when collision occurs.

--- aruco_detection/aruco_webcam.py
+++ aruco_detection/aruco_webcam.py
@@ -4,8 +4,12 @@ from cv2 import aruco
 import matplotlib.pyplot as plt
 import math
 from time import sleep
+import serial
 
 aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
+
+s = serial.Serial('com3', 9600)
+sleep(2)
 
 
 def distance(x_1, y_1, x_2, y_2):
@@ -69,14 +73,17 @@ while True:
                      (enemy.x, enemy.y), (0, 0, 255), 3)
             if distance(jack.x, jack.y, enemy.x, enemy.y) < 100:
                 print('Collision!')
+                s.write(b'1')
             else:
                 print('Detect 2 or more')
+                s.write(b'0')
     else:
         # print('Less than 2 codes')
-        pass
+        s.write(b'0')
     cv2.imshow("Webcam", frame_markers)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
+s.close()
--- bluetooth_module/test_bluetooth.ino
+++ bluetooth_module/test_bluetooth.ino
@@ -3,20 +3,20 @@
 SoftwareSerial bluetooth(10, 11);
 
 const int ledPin = 7;
-char state;
+char state = '0';
 
 void setup()
 {
     pinMode(ledPin, OUTPUT);
     digitalWrite(ledPin, LOW);
     Serial.begin(9600);
+    Serial.println("Begin!");
     bluetooth.begin(9600);
+    bluetooth.println("Begin!");
 }
 
 void loop()
 {
-    // Serial.println(Serial.read());
-    // Serial.println(state);
     if (bluetooth.available() > 0)
     {
         state = bluetooth.read();
@@ -24,80 +24,13 @@ void loop()
         if (state == '0')
         {
             digitalWrite(ledPin, LOW);
+            Serial.println("Light off");
         }
         else if (state == '1')
         {
             digitalWrite(ledPin, HIGH);
+            Serial.println("Light on");
         }
-        Serial.println(state);
     }
-
-    // if (state == 0)
-    // {
-    //     digitalWrite(ledPin, LOW);
-    //     // Serial.println("LED: OFF"); // Send back, to the phone, the String "LED: ON"
-    //     state = 0;
-    // }
-    // else if (state)
-    // {
-    //     digitalWrite(ledPin, HIGH);
-    //     Serial.println("LED: ON");
-    //     state = 0;
-    // }
+    // Serial.println(state);
 }
-
-// //Include the module so we don't
-// //have to use the default Serial
-// //so the Arduino can be plugged in
-// //to a computer and still use bluetooth
-// #include <SoftwareSerial.h>
-
-// //Define the pins used for receiving
-// //and transmitting information via Bluetooth
-// const int rxpin = 2;
-// const int txpin = 3;
-// //Variable to store input value
-// //initialized with arbitrary value
-// char k = 'A';
-// //Connect the Bluetooth module
-// SoftwareSerial bluetooth(rxpin, txpin);
-
-// //Define the pin to control the light
-// int lightbulb = 7;
-
-// void setup()
-// {
-//     //Set the lightbulb pin to put power out
-//     pinMode(lightbulb, OUTPUT);
-//     //Initialize Serial for debugging purposes
-//     Serial.begin(9600);
-//     Serial.println("Serial ready");
-//     //Initialize the bluetooth
-//     bluetooth.begin(9600);
-//     bluetooth.println("Bluetooth ready");
-// }
-
-// void loop()
-// {
-//     Serial.println(bluetooth.available())
-//         //Check for new data
-//         if (bluetooth.available())
-//     {
-//         //Remember new data
-//         k = bluetooth.read();
-//         //Print the data for debugging purposes
-//         Serial.println(k);
-//     }
-//     //Turn on the light if transmitted data is H
-//     if (k == 'H')
-//     {
-//         digitalWrite(7, HIGH);
-//     }
-//     //Turn off the light if transmitted data is L
-//     else if (k == 'L')
-//     {
-//         digitalWrite(7, LOW);
-//     }
-//     //Wait ten milliseconds to decrease unnecessary hardware strain
-//     delay(10);
-// }
