ac9e768b3d42c2ca1071ff1fdd4e1b856a144002
Huy_Surface <bicoi99@gmail.com>
2020-02-11 15:59:14 +0000

    Update wifi code for Pi to write serial to Arduino

--- wifi_communication/receive.py
+++ wifi_communication/receive.py
@@ -1,6 +1,7 @@
 # Goes on the Pi onboard
 
 import socket
+import serial
 
 Pi_IP = '192.168.137.246'
 Pi_Port = 1234
@@ -8,6 +9,9 @@ Pi_Port = 1234
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((Pi_IP, Pi_Port))
 
+s = serial.Serial('/dev/ttyACM0', 9600)
+
 while True:
     data, addr = sock.recvfrom(1024)
     print("Received:", data.decode('utf-8'))
+    s.write(data)
